d2814c7b77077fd2c4a2ae4277316edcc04bf366
IvanFoksha <v.foksh6@gmail.com>
2025-05-31 18:50:31 +0200

    Update argument - format for CLI

--- hexlet_code/scripts/gendiff.py
+++ hexlet_code/scripts/gendiff.py
@@ -5,8 +5,13 @@ def main():
     parser = argparse.ArgumentParser(
         description="Compares two configuration files and shows a difference."
     )
-    parser.add_argument('first_file', help='Path to the first configuration file')
-    parser.add_argument('second_file', help='Path to the second configuration file')
+    parser.add_argument('first_file',
+                        help='Path to the first configuration file')
+    parser.add_argument('second_file',
+                        help='Path to the second configuration file')
+    parser.add_argument('-f', '--format', metavar='FORMAT', default='stylish',
+                        help='set format of output (default: stylish)')
+
     args = parser.parse_args()
     print('Gendiff is ready to rock! Use -h for help')
 
