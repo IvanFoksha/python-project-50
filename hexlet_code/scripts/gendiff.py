fdab3e933eeb3710f8a406b76f4f816b09191272
IvanFoksha <v.foksh6@gmail.com>
2025-05-31 19:18:41 +0200

    Update gendiff: add read_file

--- hexlet_code/scripts/gendiff.py
+++ hexlet_code/scripts/gendiff.py
@@ -1,4 +1,6 @@
 import argparse
+import json
+from pathlib import Path
 
 
 def main():
@@ -14,6 +16,16 @@ def main():
 
     args = parser.parse_args()
     print('Gendiff is ready to rock! Use -h for help')
+
+
+def read_file(file_path):
+    file_ext = Path(file_path).suffix.lower()
+
+    if file_ext == '.json':
+        with open(file_path, 'r') as f:
+            return json.load(f)
+    else:
+        raise ValueError(f'Unsupported file format: {file_ext}')
 
 
 if __name__ == '__main__':
