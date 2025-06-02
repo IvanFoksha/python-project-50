5e3ffd3ef891299b225f5ff4ff53f113d9fe083f
IvanFoksha <v.foksh6@gmail.com>
2025-06-02 08:47:07 +0200

    Fix test_gendiff: added read_json and fix read_file after test

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -8,12 +8,17 @@ FIXTURES_PATH = Path(__file__).parent / 'test_data'
 
 def read_file(file_path):
     with open(file_path, 'r') as f:
+        return f.read()
+
+
+def read_json(path):
+    with open(path, 'r') as f:
         return json.load(f)
 
 
 def test_gendiff_json():
-    data1 = read_file(FIXTURES_PATH / 'file1.json')
-    data2 = read_file(FIXTURES_PATH / 'file2.json')
+    data1 = read_json(FIXTURES_PATH / 'file1.json')
+    data2 = read_json(FIXTURES_PATH / 'file2.json')
 
     result = generate_diff(data1, data2)
     expected = read_file(FIXTURES_PATH / 'expected_result.txt').strip()
