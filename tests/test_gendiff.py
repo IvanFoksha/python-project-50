36bbce461783010f2a78751856c718bb40a75598
IvanFoksha <v.foksh6@gmail.com>
2025-06-02 08:35:01 +0200

    Fix test_gendiff: read_file

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,3 +1,4 @@
+import json
 from pathlib import Path
 
 from hexlet_code import generate_diff
@@ -5,9 +6,9 @@ from hexlet_code import generate_diff
 FIXTURES_PATH = Path(__file__).parent / 'test_data'
 
 
-def read_file(file_name):
-    with open(FIXTURES_PATH / file_name, 'r') as f:
-        return f.read()
+def read_file(file_path):
+    with open(file_path, 'r') as f:
+        return json.load(f)
 
 
 def test_gendiff_json():
@@ -15,6 +16,6 @@ def test_gendiff_json():
     data2 = read_file(FIXTURES_PATH / 'file2.json')
 
     result = generate_diff(data1, data2)
-
     expected = read_file(FIXTURES_PATH / 'expected_result.txt').strip()
+
     assert result.strip() == expected
