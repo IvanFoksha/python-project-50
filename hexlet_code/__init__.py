1a22327c8df3d58ee2503a69c2370404154633d6
IvanFoksha <v.foksh6@gmail.com>
2025-06-04 11:29:37 +0200

    Update init file in hexlet_code for recursive comparison

--- hexlet_code/__init__.py
+++ hexlet_code/__init__.py
@@ -1,1 +1,15 @@
-from .scripts.gendiff import generate_diff
+from .diff_builder import build_diff_tree
+from .formatters.stylish import format_output
+from .parser import parse_file
+
+
+def generate_diff(file_path1, file_path2, format_name='stylish'):
+    data1 = parse_file(file_path1)
+    data2 = parse_file(file_path2)
+
+    diff_tree = build_diff_tree(data1, data2)
+
+    if format_name == 'stylish':
+        return format_output(diff_tree)
+    else:
+        raise ValueError(f'Unsupported format: {format_name}')
