c95c279c971f85ae456b0f8528ed458a117405ca
IvanFoksha <v.foksh6@gmail.com>
2025-06-10 13:12:26 +0200

    Create test on plain formatter

---
+++ tests/test_plain_diff.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+
+from gendiff import generate_diff_from_data
+from gendiff.parser import parse_file
+
+FIXTURES_PATH = Path(__file__).parent / 'test_data'
+
+
+def read_file(path):
+    with open(path, 'r') as f:
+        return f.read()
+
+
+def test_gendiff_json_plain():
+    data1 = parse_file(FIXTURES_PATH / 'file1.json')
+    data2 = parse_file(FIXTURES_PATH / 'file2.json')
+
+    result = generate_diff_from_data(data1, data2, 'plain')
+    expected = read_file(FIXTURES_PATH / 'expected_result_plain.txt').strip()
+    assert result.strip() == expected
+
+
+# def test_gendiff_yaml_plain():
+#     data1 = parse_file(FIXTURES_PATH / 'file1.yml')
+#     data2 = parse_file(FIXTURES_PATH / 'file2.yaml')
+
+#     result = generate_diff_from_data(data1, data2, 'plain')
+#     expected = read_file(FIXTURES_PATH /
+# 'expected_result_stylish.txt').strip()
+#     assert result.strip() == expected
