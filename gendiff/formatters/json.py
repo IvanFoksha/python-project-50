4f04272323bac7ef8956836641be671921bbcd5e
IvanFoksha <v.foksh6@gmail.com>
2025-06-11 10:21:27 +0200

    Update json formatter after tests

--- gendiff/formatters/json.py
+++ gendiff/formatters/json.py
@@ -1,8 +1,6 @@
 import json
 
-from .stylish import format_value
-
 
 def format_json(diff_tree):
     return json.dumps(diff_tree, indent=2,
-                      default=format_value, sort_keys=False)
+                      ensure_ascii=False, sort_keys=False)
