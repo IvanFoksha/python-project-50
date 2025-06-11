41da7986cc01fd8d0b31a6c17d0cecc6fc7c0d6a
IvanFoksha <v.foksh6@gmail.com>
2025-06-11 10:19:17 +0200

    Update init file for formatters

--- gendiff/__init__.py
+++ gendiff/__init__.py
@@ -5,21 +5,29 @@ from .formatters.stylish import format_stylish
 from .parser import parse_file
 
 
+FORMATTERS = {
+    'stylish': lambda diff: format_stylish(diff, depth=1),
+    'plain': format_plain,
+    'json': format_json,
+}
+
+
 def generate_diff(file_path1, file_path2, format_name='stylish'):
     data1 = parse_file(file_path1)
     data2 = parse_file(file_path2)
 
     diff_tree = build_diff_tree(data1, data2)
 
-    if format_name == 'stylish':
-        inner = format_stylish(diff_tree, depth=1)
-        return '{\n' + inner + '\n}'
-    if format_name == 'plain':
-        return format_plain(diff_tree)
-    if format_name == 'json':
-        return format_json(diff_tree)
-    else:
+    if format_name not in FORMATTERS:
         raise ValueError(f'Unsupported format: {format_name}')
+
+    formatter = FORMATTERS[format_name]
+    result = formatter(diff_tree)
+
+    if format_name == 'stylish':
+        return '{\n' + result + '\n}'
+    else:
+        return result
 
 
 def generate_diff_from_data(data1, data2, format_name='stylish'):
