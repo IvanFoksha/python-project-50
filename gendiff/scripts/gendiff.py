4cb6894382ee72fb3c686af933173dda9e7b4c5a
IvanFoksha <v.foksh6@gmail.com>
2025-06-12 10:14:21 +0200

    Removed main and update gendiff: delete "#" text and configured path

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,46 +1,6 @@
 import argparse
 
-from ..__init__ import generate_diff
-
-# def read_file(file_path):
-
-#     file_ext = Path(file_path).suffix.lower()
-
-#     if file_ext == '.json':
-#         with open(file_path, 'r') as f:
-#             return json.load(f)
-#     else:
-#         raise ValueError(f'Unsupported file format: {file_ext}')
-
-
-# def format_value(value):
-#     if isinstance(value, bool):
-#         return 'true' if value else 'false'
-#     elif value is None:
-#         return 'null'
-#     else:
-#         return str(value)
-
-
-# def generate_diff(data1, data2):
-#     keys = sorted(set(data1.keys()) | set(data2.keys()))
-#     lines = []
-
-#     for key in keys:
-#         val1 = data1.get(key)
-#         val2 = data2.get(key)
-
-#         if key in data1 and key not in data2:
-#             lines.append(f'- {key}: {format_value(val1)}')
-#         elif key in data2 and key not in data1:
-#             lines.append(f'+ {key}: {format_value(val2)}')
-#         elif val1 != val2:
-#             lines.append(f'- {key}: {format_value(val1)}')
-#             lines.append(f'+ {key}: {format_value(val2)}')
-#         else:
-#             lines.append(f'  {key}: {format_value(val1)}')
-
-#     return '{\n' + '\n'.join(f'  {line}' for line in lines) + '\n}'
+from gendiff.core import generate_diff
 
 
 def main():
